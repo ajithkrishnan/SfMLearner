2c325c841601b15aab71411f007bdabd314a42ed
ajithkrishnan <ajithkrishnanbm@gmail.com>
2019-03-11 05:21:53 +0000

    added plot flag

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-DATASET_DIR=~/tensorflow/thesis/models/research/vid2depth/data/dataset
-OUTPUT_DIR=$(pwd)/test_output
+export PATH_VID2DEPTH=~/tensorflow/thesis/models/research/vid2depth
+export OUTPUT_DIR=$(pwd)/test_output_1103
 
 python3 test_kitti_pose.py \
     --test_seq 9 \
-    --dataset_dir $DATASET_DIR/ \
+    --dataset_dir $PATH_VID2DEPTH/data/dataset/ \
     --plot True \
     --output_dir $OUTPUT_DIR/ \
     --ckpt_file models/model-100280
